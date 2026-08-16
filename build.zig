1f51c82ccdec2dd228e562eeff662b25b6a3cace
Tobias Simetsreiter <dasimmet@gmail.com>
2026-08-16 09:54:18 +0200

    make picotool dep non lazy

--- build.zig
+++ build.zig
@@ -84,205 +84,202 @@ pub fn build(b: *std.Build) void {
         &b.addInstallArtifact(binh, .{}).step,
     );
 
-    if (b.lazyDependency("picotool", .{
+    const picotool_src = b.dependency("picotool", .{});
+
+    const generate_headers = b.step(
+        "generate_headers",
+        "install the generated binh headers",
+    );
+    const xip_ram_perms_elf_h = generate_binh(
+        b,
+        binh,
+        picotool_src.path("xip_ram_perms/xip_ram_perms.elf"),
+        "xip_ram_perms_elf",
+        "xip_ram_perms_elf.h",
+    );
+    generate_headers.dependOn(&b.addInstallHeaderFile(
+        xip_ram_perms_elf_h,
+        "xip_ram_perms_elf.h",
+    ).step);
+
+    const flash_id_bin_h = generate_binh(
+        b,
+        binh,
+        picotool_src.path("picoboot_flash_id/flash_id.bin"),
+        "flash_id_bin",
+        "flash_id_bin.h",
+    );
+    generate_headers.dependOn(&b.addInstallHeaderFile(
+        flash_id_bin_h,
+        "flash_id_bin.h",
+    ).step);
+
+    const data_locs_opt = b.option(
+        []const u8,
+        "data-locs",
+        "semicolon separated runtime data locations",
+    ) orelse "./;/usr/local/share/picotool";
+
+    const data_locs_vec = b.fmt("{f}", .{
+        DataLocsFmt.fmt(data_locs_opt),
+    });
+
+    const data_locs = b.addConfigHeader(.{
+        .style = .{ .cmake = picotool_src.path("data_locs.template.cpp") },
+        .include_path = "data_locs.cpp",
+    }, .{
+        .DATA_LOCS_VEC = data_locs_vec,
+    });
+
+    //elf2uf2
+    const elf2uf2 = b.createModule(.{
         .target = target,
         .optimize = optimize,
-    })) |picotool_src| {
-        const generate_headers = b.step(
-            "generate_headers",
-            "install the generated binh headers",
-        );
-        const xip_ram_perms_elf_h = generate_header(
-            b,
-            binh,
-            picotool_src.path("xip_ram_perms/xip_ram_perms.elf"),
-            "xip_ram_perms_elf",
-            "xip_ram_perms_elf.h",
-        );
-        generate_headers.dependOn(&b.addInstallFile(
-            xip_ram_perms_elf_h,
-            "include/xip_ram_perms_elf.h",
-        ).step);
+        .link_libcpp = true,
+    });
+    const libelf2uf2 = b.addLibrary(.{
+        .name = "elf2uf2",
+        .root_module = elf2uf2,
+    });
 
-        const flash_id_bin_h = generate_header(
-            b,
-            binh,
-            picotool_src.path("picoboot_flash_id/flash_id.bin"),
-            "flash_id_bin",
-            "flash_id_bin.h",
-        );
-        generate_headers.dependOn(&b.addInstallFile(
-            flash_id_bin_h,
-            "include/flash_id_bin.h",
-        ).step);
+    elf2uf2.addCSourceFiles(.{
+        .files = &.{"elf2uf2.cpp"},
+        .root = picotool_src.path("elf2uf2"),
+        .flags = &cppflags,
+    });
 
-        const data_locs_opt = b.option(
-            []const u8,
-            "data-locs",
-            "semicolon separated runtime data locations",
-        ) orelse "./;/usr/local/share/picotool";
-
-        const data_locs_vec = b.fmt("{f}", .{
-            DataLocsFmt.fmt(data_locs_opt),
-        });
-
-        const data_locs = b.addConfigHeader(.{
-            .style = .{ .cmake = picotool_src.path("data_locs.template.cpp") },
-            .include_path = "data_locs.cpp",
-        }, .{
-            .DATA_LOCS_VEC = data_locs_vec,
-        });
-
-        //elf2uf2
-        const elf2uf2 = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libcpp = true,
-        });
-        const libelf2uf2 = b.addLibrary(.{
-            .name = "elf2uf2",
-            .root_module = elf2uf2,
-        });
-
-        elf2uf2.addCSourceFiles(.{
-            .files = &.{"elf2uf2.cpp"},
-            .root = picotool_src.path("elf2uf2"),
-            .flags = &cppflags,
-        });
-
-        inline for (.{
-            "elf",
-            "errors",
-            "model",
-        }) |include_path| {
-            const picotool_path = picotool_src.path(include_path);
-            elf2uf2.addIncludePath(picotool_path);
-        }
-        inline for (.{
-            "src/common/boot_picoboot_headers/include",
-            "src/common/boot_uf2_headers/include",
-            "src/host/pico_platform/include",
-        }) |include_path| {
-            const pico_sdk_path = pico_sdk.path(include_path);
-            elf2uf2.addIncludePath(pico_sdk_path);
-        }
-
-        //picotool
-        const picotool = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-        });
-        const picotool_exe = b.addExecutable(.{
-            .name = "picotool",
-            .root_module = picotool,
-        });
-
-        picotool.addCSourceFile(.{
-            .file = data_locs.getOutputFile(),
-            .flags = &cppflags,
-        });
-        picotool.addCSourceFiles(.{
-            .files = &.{
-                "main.cpp",
-                "model/model.cpp",
-                "otp.cpp",
-                "get_xip_ram_perms.cpp",
-                "bintool/bintool.cpp",
-                "elf/elf_file.cpp",
-                "errors/errors.cpp",
-                "lib/whereami/whereami++.cpp",
-                "picoboot_connection/picoboot_connection_cxx.cpp",
-            },
-            .root = picotool_src.path(""),
-            .flags = &cppflags,
-        });
-        picotool.addCSourceFiles(.{
-            .files = &.{
-                "picoboot_connection/picoboot_connection.c",
-            },
-            .root = picotool_src.path(""),
-            .flags = &cflags,
-        });
-
-        inline for (.{
-            .{ "SYSTEM_VERSION", "\"" ++ pico_sdk_version ++ "\"" },
-            .{ "PICOTOOL_VERSION", "\"" ++ picotool_version ++ "\"" },
-            .{ "COMPILER_INFO", "\"zig-" ++ builtin.zig_version_string ++ "\"" },
-            .{ "_CLANG_DISABLE_CRT_DEPRECATION_WARNINGS", "1" },
-        }) |macro| {
-            picotool.addCMacro(macro[0], macro[1]);
-        }
-
-        picotool.addCMacro("HAS_LIBUSB", "1");
-        picotool.linkLibrary(libusb.artifact("usb"));
-        picotool.linkLibrary(libelf2uf2);
-
-        inline for (&.{
-            "rp2350_a2_rom_end",
-            "rp2350_a3_rom_end",
-            "rp2350_a4_rom_end",
-        }) |bin_h_name| {
-            const rp2350_h = generate_header(
-                b,
-                binh,
-                picotool_src.path("model/" ++ bin_h_name ++ ".bin"),
-                bin_h_name,
-                bin_h_name ++ ".h",
-            );
-            generate_headers.dependOn(&b.addInstallFile(
-                rp2350_h,
-                "include/rp2350.rom.h",
-            ).step);
-            picotool.addIncludePath(rp2350_h.dirname());
-            elf2uf2.addIncludePath(rp2350_h.dirname());
-        }
-
-        picotool.addIncludePath(xip_ram_perms_elf_h.dirname());
-        picotool.addIncludePath(flash_id_bin_h.dirname());
-
-        inline for (.{
-            "",
-            "bintool",
-            "elf",
-            "elf2uf2",
-            "errors",
-            "lib/nlohmann_json/single_include",
-            "lib/whereami",
-            "model",
-            "otp_header_parser",
-            "picoboot_connection",
-        }) |include_path| {
-            const picotool_path = picotool_src.path(include_path);
-            picotool.addIncludePath(picotool_path);
-        }
-
-        inline for (.{
-            "src/common/boot_picobin_headers/include",
-            "src/common/boot_picoboot_headers/include",
-            "src/common/boot_uf2_headers/include",
-            "src/common/pico_binary_info/include",
-            "src/common/pico_usb_reset_interface_headers/include",
-            "src/host/pico_platform/include",
-            "src/rp2_common/boot_bootrom_headers/include",
-            "src/rp2_common/pico_stdio_usb/include",
-            "src/rp2350/hardware_regs/include",
-        }) |include_path| {
-            const pico_sdk_path = pico_sdk.path(include_path);
-            picotool.addIncludePath(pico_sdk_path);
-        }
-        b.installArtifact(picotool_exe);
-
-        const run_picotool = b.addRunArtifact(picotool_exe);
-        passthroughArgs(b, run_picotool);
-        run_step.dependOn(&run_picotool.step);
-
-        const udev_rules = b.addInstallFile(
-            picotool_src.path("udev/60-picotool.rules"),
-            "etc/udev/rules.d/60-picotool.rules",
-        );
-        b.getInstallStep().dependOn(&udev_rules.step);
-        b.step("udev", "install the raspberry udev rules").dependOn(&udev_rules.step);
+    inline for (.{
+        "elf",
+        "errors",
+        "model",
+    }) |include_path| {
+        elf2uf2.addIncludePath(picotool_src.path(include_path));
     }
+
+    inline for (.{
+        "src/common/boot_picoboot_headers/include",
+        "src/common/boot_uf2_headers/include",
+        "src/host/pico_platform/include",
+    }) |include_path| {
+        elf2uf2.addIncludePath(pico_sdk.path(include_path));
+    }
+
+    //picotool
+    const picotool = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+    });
+    const picotool_exe = b.addExecutable(.{
+        .name = "picotool",
+        .root_module = picotool,
+    });
+
+    picotool.addCSourceFile(.{
+        .file = data_locs.getOutputFile(),
+        .flags = &cppflags,
+    });
+    picotool.addCSourceFiles(.{
+        .files = &.{
+            "main.cpp",
+            "model/model.cpp",
+            "otp.cpp",
+            "get_xip_ram_perms.cpp",
+            "bintool/bintool.cpp",
+            "elf/elf_file.cpp",
+            "errors/errors.cpp",
+            "lib/whereami/whereami++.cpp",
+            "picoboot_connection/picoboot_connection_cxx.cpp",
+        },
+        .root = picotool_src.path(""),
+        .flags = &cppflags,
+    });
+    picotool.addCSourceFiles(.{
+        .files = &.{
+            "picoboot_connection/picoboot_connection.c",
+        },
+        .root = picotool_src.path(""),
+        .flags = &cflags,
+    });
+
+    inline for (.{
+        .{ "SYSTEM_VERSION", "\"" ++ pico_sdk_version ++ "\"" },
+        .{ "PICOTOOL_VERSION", "\"" ++ picotool_version ++ "\"" },
+        .{ "COMPILER_INFO", "\"zig-" ++ builtin.zig_version_string ++ "\"" },
+        .{ "_CLANG_DISABLE_CRT_DEPRECATION_WARNINGS", "1" },
+    }) |macro| {
+        picotool.addCMacro(macro[0], macro[1]);
+    }
+
+    picotool.addCMacro("HAS_LIBUSB", "1");
+    picotool.linkLibrary(libusb.artifact("usb"));
+    picotool.linkLibrary(libelf2uf2);
+
+    inline for (&.{
+        "rp2350_a2_rom_end",
+        "rp2350_a3_rom_end",
+        "rp2350_a4_rom_end",
+    }) |bin_h_name| {
+        const model_h_name = bin_h_name ++ ".h";
+        const model_h = generate_binh(
+            b,
+            binh,
+            picotool_src.path("model/" ++ bin_h_name ++ ".bin"),
+            bin_h_name,
+            model_h_name,
+        );
+        generate_headers.dependOn(&b.addInstallHeaderFile(
+            model_h,
+            model_h_name,
+        ).step);
+        picotool.addIncludePath(model_h.dirname());
+        elf2uf2.addIncludePath(model_h.dirname());
+    }
+
+    picotool.addIncludePath(xip_ram_perms_elf_h.dirname());
+    picotool.addIncludePath(flash_id_bin_h.dirname());
+
+    inline for (.{
+        "",
+        "bintool",
+        "elf",
+        "elf2uf2",
+        "errors",
+        "lib/nlohmann_json/single_include",
+        "lib/whereami",
+        "model",
+        "otp_header_parser",
+        "picoboot_connection",
+    }) |include_path| {
+        const picotool_path = picotool_src.path(include_path);
+        picotool.addIncludePath(picotool_path);
+    }
+
+    inline for (.{
+        "src/common/boot_picobin_headers/include",
+        "src/common/boot_picoboot_headers/include",
+        "src/common/boot_uf2_headers/include",
+        "src/common/pico_binary_info/include",
+        "src/common/pico_usb_reset_interface_headers/include",
+        "src/host/pico_platform/include",
+        "src/rp2_common/boot_bootrom_headers/include",
+        "src/rp2_common/pico_stdio_usb/include",
+        "src/rp2350/hardware_regs/include",
+    }) |include_path| {
+        const pico_sdk_path = pico_sdk.path(include_path);
+        picotool.addIncludePath(pico_sdk_path);
+    }
+    b.installArtifact(picotool_exe);
+
+    const run_picotool = b.addRunArtifact(picotool_exe);
+    passthroughArgs(b, run_picotool);
+    run_step.dependOn(&run_picotool.step);
+
+    const udev_rules = b.addInstallFile(
+        picotool_src.path("udev/60-picotool.rules"),
+        "etc/udev/rules.d/60-picotool.rules",
+    );
+    b.getInstallStep().dependOn(&udev_rules.step);
+    b.step("udev", "install the raspberry udev rules").dependOn(&udev_rules.step);
 }
 
 pub const DataLocsFmt = struct {
@@ -336,7 +333,7 @@ pub const commonflags = .{
     "-Wno-zero-length-array",
 };
 
-pub fn generate_header(
+pub fn generate_binh(
     b: *std.Build,
     binh: *std.Build.Step.Compile,
     input: std.Build.LazyPath,
